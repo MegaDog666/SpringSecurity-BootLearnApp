5fc6991766a62defca5cb661bd2a469c21eac28d
MegaDog666 <Moonandre9999@mail.ru>
2024-12-07 01:36:32 +0300

    Les 80 (Spring Security)
    Нууу... Урок короткий, 30 минут прошло, понимание есть, с моим кодом все сходится, он начал делать шифрование как я (Ну точнее убрал его вообще, объяснил, но я это все знал :))
    В общем все понятно, не буду много говорить тут, много сказал на том коммите, пойду спать, мне завтра утром вставать, я считаю провел с пользой это время

--- src/main/java/org/example/FirstSpringSecurityApp/config/SecurityConfig.java
+++ src/main/java/org/example/FirstSpringSecurityApp/config/SecurityConfig.java
@@ -1,6 +1,6 @@
 package org.example.FirstSpringSecurityApp.config;
 
-import org.example.FirstSpringSecurityApp.security.AuthProviderImlp;
+import org.example.FirstSpringSecurityApp.services.PersonDetailsService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -9,7 +9,6 @@ import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
@@ -18,11 +17,10 @@ import org.springframework.security.web.SecurityFilterChain;
 @EnableWebSecurity
 public class SecurityConfig {
 
-    private final AuthProviderImlp auth;
+    private final PersonDetailsService personDetailsService;
 
-    @Autowired
-    public SecurityConfig(@Lazy AuthProviderImlp auth) {
-        this.auth = auth;
+    public SecurityConfig(PersonDetailsService personDetailsService) {
+        this.personDetailsService = personDetailsService;
     }
 
     @Bean
@@ -49,7 +47,7 @@ public class SecurityConfig {
     @Bean
     public AuthenticationManager authenticationManager(HttpSecurity http) throws Exception {
         AuthenticationManagerBuilder authenticationManagerBuilder = http.getSharedObject(AuthenticationManagerBuilder.class);
-        authenticationManagerBuilder.authenticationProvider(auth);
+        authenticationManagerBuilder.userDetailsService(personDetailsService);
         return authenticationManagerBuilder.build();
     }
 
