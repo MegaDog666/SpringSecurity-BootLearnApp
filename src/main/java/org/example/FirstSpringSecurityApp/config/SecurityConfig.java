960e0e14a82d520c7088281b33ebd67766c19a67
MegaDog666 <Moonandre9999@mail.ru>
2024-12-07 22:01:26 +0300

    Les 82 (Spring Security)
    Чтож, интересный урок, правда, я многое повспоминал, думаю на сегодня хватит уроков)
    Хорошо день провел, прям мне все нравится, я все понимаю, пошла белая, райская полоса, и я хочу правда уже до конца дойти, тут чуток осталось, 97 уроков всего, так что, думаю я уже 90% прошел)

--- src/main/java/org/example/FirstSpringSecurityApp/config/SecurityConfig.java
+++ src/main/java/org/example/FirstSpringSecurityApp/config/SecurityConfig.java
@@ -29,7 +29,7 @@ public class SecurityConfig {
                 .csrf(csrf -> csrf.disable())
 
                 .authorizeHttpRequests(authorize -> authorize
-                        .requestMatchers("/auth/login", "/error").permitAll()
+                        .requestMatchers("/auth/login", "/auth/registration", "/error").permitAll()
                         .anyRequest().authenticated()
                 )
 
