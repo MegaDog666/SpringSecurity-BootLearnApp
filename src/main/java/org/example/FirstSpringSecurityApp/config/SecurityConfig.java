ceecbd4b62817ac4a6279912fc6d6dc2a1f0ed30
MegaDog666 <Moonandre9999@mail.ru>
2024-12-13 17:04:12 +0300

    Les 87 (Spring Security)
    В общем и целом понятно, было трудно тогда, щас проще, уже типо что-то сложное упростили и сделали все через аннотации, немного плохо усвоилось... Чувствую это, но я не хочу щас 20 минут тратить на это... Правда, итак уже долго все изучаю, в общем и целом, все так понятно)
    А так не трудно, и вроде юнит с Spring Security закончился, походу... Блин ну он быстро прошел мне кажется плохо усвоился...
    Ладно дальше есть Spring REST, предполагаю что-то будет с API, с ними я работал уже, поэтому тут попроще будет)

--- src/main/java/org/example/FirstSpringSecurityApp/config/SecurityConfig.java
+++ src/main/java/org/example/FirstSpringSecurityApp/config/SecurityConfig.java
@@ -7,6 +7,8 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Lazy;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.authentication.configuration.EnableGlobalAuthentication;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
@@ -16,6 +18,7 @@ import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 @EnableWebSecurity
+@EnableGlobalMethodSecurity(prePostEnabled = true)
 public class SecurityConfig {
 
     private final PersonDetailsService personDetailsService;
@@ -28,7 +31,6 @@ public class SecurityConfig {
     protected SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
                 .authorizeHttpRequests(authorize -> authorize
-                        .requestMatchers("/admin").hasRole("ADMIN")
                         .requestMatchers("/auth/login", "/auth/registration", "/error").permitAll()
                         .anyRequest().hasAnyRole("USER", "ADMIN")
 
